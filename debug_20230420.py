e725ae624140be62b047b5d8192c7ef0d6e86fe7
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-04-20 16:18:23 +0200

    hello wordl

--- debug_20230420.py
+++ debug_20230420.py
@@ -1,9 +1,13 @@
 from import_data import import_raw
 from fractions_data import get_fractions
+import porespy as ps
+import openpnm as op
 
 dir_path_1_12_100cube = './subvolume/Sandstone_1_29_100cube.raw'
 data_1_12_100cube = import_raw(dir_path_1_12_100cube, 100)
 fractions = get_fractions(data_1_12_100cube)
 print(fractions)
 
-# hello user-1
+snow_output = ps.networks.snow2(data_1_12_100cube, voxel_size=2.8)
+pn = op.io.network_from_porespy(snow_output.network)
+print(pn)
