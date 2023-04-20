5570ccaabc66c5ad232f6e316ae1c0b092c629ad
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-04-20 17:12:27 +0200

    update: 2023-04-20

--- debug_20230417.py
+++ debug_20230417.py
@@ -1,16 +1,3 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
 from import_data import import_raw
 
-dir_path_1_12_800cube = './data/Sandstone_1_12_800cube_raw.raw'
-data_1_12_800cube_raw = import_raw(dir_path_1_12_800cube, 800)
-xy_raw = data_1_12_800cube_raw[:, :, 1]
-plt.hist(xy_raw.flat, bins=200, range=(0, 255))
-plt.show()
-
-dir_path_1_12_800cube = './data/Sandstone_1_12_800cube_nlm.raw'
-data_1_12_800cube_nlm = import_raw(dir_path_1_12_800cube, 800)
-xy_raw = data_1_12_800cube_nlm[:, :, 1]
-plt.hist(xy_raw.flat, bins=200, range=(0, 255))
-plt.show()
+data = import_raw(path='./subvolume/Sandstone_1_29_100cube.raw', dimension=100)
