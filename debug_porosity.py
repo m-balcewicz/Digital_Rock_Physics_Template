7c96932636f841cc682c5f7c906b3c8891716494
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-04-20 18:35:56 +0200

    update: 2023-04-20

--- debug_porosity.py
+++ debug_porosity.py
@@ -15,6 +15,30 @@ print(fractions)
 snow_output = ps.networks.snow2(data_100cube, voxel_size=2.8)
 pn = op.io.network_from_porespy(snow_output.network)
 print(pn)
+
+# sphinx_gallery_thumbnail_number = 2
+import spam.mesh
+import matplotlib.pyplot as plt
+import spam.label
+import spam.plotting
+import scipy.ndimage
+import spam.datasets
+import numpy
+
+midSlice = data_100cube.shape[0] // 2
+binary = numpy.logical_and(data_100cube > 28000, data_100cube < 40000)
+cyl = spam.mesh.createCylindricalMask(data_100cube.shape, 40)
+binary = numpy.logical_and(binary, cyl)
+pores = scipy.ndimage.binary_dilation(data_100cube < 26000, iterations=1)
+binary[pores] = 0
+labelled = spam.label.ITKwatershed.watershed(binary)
+print("{} particles have been identified".format(labelled.max()))
+plt.figure()
+plt.title("Labelled volume")
+plt.imshow(labelled[midSlice], cmap=spam.label.randomCmap)
+plt.savefig('spam-figure-2.png', format='png')
+
+
 """
 # Save the data as a text file
 np.savetxt("data.txt", data, header=header, fmt=fmt, delimiter="|")
