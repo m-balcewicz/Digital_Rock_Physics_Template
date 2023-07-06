863b20fd612c0ec9e5630a87aafc2a57fdc4c947
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-07-06 09:35:06 +0200

    regular updates

--- projects/debugging/debug_import_and_export.py
+++ projects/debugging/debug_import_and_export.py
@@ -1,18 +1,18 @@
+import os
 from drp_template import import_export as ie
 from drp_template.plot_save_figure import plot_slice
 
-path_in = '/Volumes/mbalcewicz/SCIENCE_WORLD/STUDIES/2023_PYTHON_POROSITY/PerGeos/PerGeos_EXPORT'
+path_in = '/Volumes/files/mbalcewicz/SCIENCE_WORLD/STUDIES/2023_PYTHON_POROSITY/PerGeos/PerGeos_EXPORT'
 path_out = '/Volumes/mbalcewicz/SCIENCE_WORLD/STUDIES/2023_PYTHON_POROSITY/PYTHON/PYTHON_EXPORT'
-data_raw = ie.import_raw(f'{path_in}/100cube.raw', dimension=100)
-
+data_raw = ie.import_raw(os.path.join(path_in, '100cube_raw.raw'), dimension=100, dtype='uint8')
+data_binary = ie.import_raw(os.path.join(path_in, '100cube_binary.raw'), dimension=100, dtype='uint8')
 
 slice_no = 49  # This is a characteristic 2D within the 3D volume
 fig_data_1_12_100cube_xy = plot_slice(data_raw, slice=slice_no, plane='xy', title='XY')
 fig_data_1_12_100cube_xy.show()
-fig_data_1_12_100cube_xy = plot_slice(data_raw, slice=slice_no, plane='xz', title=f'XZ')
+fig_data_1_12_100cube_xy = plot_slice(data_binary, slice=slice_no, plane='xy', title='XY')
 fig_data_1_12_100cube_xy.show()
-fig_data_1_12_100cube_xy = plot_slice(data_raw, slice=slice_no, plane='yz', title='YZ')
-fig_data_1_12_100cube_xy.show()
+
 
 # ie.export_raw(data_raw, path=path_out, varname='export_raw')
 # ie.export_3d_tif(data_raw, path=path_out, varname='export_tif')
