35e8a46f7adfd6b0dab8abecce9edf30bf6e3d3e
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-10-10 08:03:42 +0200

    updates

--- examples/example_2D_tif_stack.py
+++
@@ -1,9 +0,0 @@
-import barbara.drp_template.import_export as ie
-from barbara.drp_template import plot_slice
-
-data = ie.import_2d_tiff(path='/Volumes/files/mbalcewicz/XRAY_COMPUTED_TOMOGRAPHY/2022_CONCRETE/CoDA/PLUGS_SMALL/S-4-X/04_Perth/0_1MPa/S-4-X_0_1MPa_1_9999um', type='raw')
-plane = 'xy'
-
-fig = plot_slice(data=data, cmap_set='helix', slice=0, plane=plane)
-fig.show()
-
