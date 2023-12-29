874c2fe0308014771d0bfc46b8e2ece434cb98f7
m-balcewicz <martin.balcewicz@rockphysics.org>
2023-12-29 11:33:04 +0100

    update & cleanup

--- drp_template/drp_template/__init__.py
+++ drp_template/drp_template/__init__.py
@@ -10,6 +10,6 @@ from .directory_information import get_dir_info
 from .import_export import import_raw, import_2d_tiff, import_3d_tiff, import_moduli, import_test, export_raw, export_2d_tif, export_3d_tif, \
     export_vtk
 from .plot_save_figure import plot_slice
-from .change_endianess import little_to_big, big_to_little
+from ..tools.change_endianess import little_to_big, big_to_little
 from .fractions_data import get_fractions
 from .label_data import label_binary
--- drp_template/tools/_funcs.py
+++ drp_template/tools/_funcs.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 __all__ = [
-    'check_binary'
+    'check_binary',
+    'list_dir_info',
+    'list_dir_info'
 ]
 
 from drp_template.default_params import print_style
@@ -30,3 +32,46 @@ def check_binary(model, filename):
         model = model - 1
 
     return model
+
+def get_dir_info(directory):
+    file_info_list = []
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            file_info = {}
+            file_info['filename'] = filename
+            file_info['filetype'] = os.path.splitext(filename)[1]
+            file_info['filesize_bytes'] = os.path.getsize(file_path)
+            file_info['filesize_megabytes'] = file_info['filesize_bytes'] / 1000000.0
+            file_info['full_path'] = os.path.abspath(file_path)
+            file_info_list.append(file_info)
+
+    file_info_list = pd.DataFrame(file_info_list)
+    return file_info_list
+
+
+def list_dir_info(directory, extension=None):
+    """
+    List all files with a specific extension in a directory,
+    or list all subfolders if no extension is provided.
+
+    Args:
+        directory (str): The directory to search for files and subfolders.
+        extension (str, optional): The file extension to filter the files.
+                                   Default is None.
+
+    Returns:
+        list: A list of file names with the specified extension in the directory,
+              or a list of subfolder names in the directory.
+
+    """
+    directory_listing = []
+    for entry in os.scandir(directory):
+        if entry.is_dir():
+            directory_listing.append(entry.name)
+
+    if extension is not None:
+        directory_listing = [file for file in directory_listing
+                             if any(file.endswith(extension) for file in os.listdir(os.path.join(directory, file)))]
+
+    return directory_listing
