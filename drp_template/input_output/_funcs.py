f23f6248cbdaa36516a559071184d3be267b4d29
m-balcewicz <martin.balcewicz@rockphysics.org>
2024-05-27 14:40:58 +0200

    feat: Add check_output_folder function and update import_tiff_sequence
    The code changes in this commit add a new function `check_output_folder` to the `_funcs.py` file. This function checks if the output folder exists and creates it if it doesn't. It returns the path to the output folder.
    The `import_tiff_sequence` function is also updated to include an optional `dimensions` parameter. This allows for specifying the dimensions of the model when importing a TIFF sequence. The function now calls the `mk_paramsfile` function to create the parameters file and updates it with the dimensions.
    
    These changes enhance the functionality of the codebase by providing a convenient way to check and create the output folder, as well as specifying the dimensions when importing a TIFF sequence.
    
    This commit message follows the established convention of using a prefix to indicate the type of change, followed by a concise and descriptive message.

--- drp_template/input_output/_funcs.py
+++ drp_template/input_output/_funcs.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 from tifffile import tifffile
 
-from drp_template.default_params import update_parameters_file
-from drp_template.tools import check_binary
+from drp_template.default_params import update_parameters_file, check_output_folder
+from drp_template.tools import check_binary, mk_paramsfile, get_model_dimensions, reshape_model
 from drp_template.tools.dirify import mk_dir
 from drp_template.tools.logify import mk_log
 import skimage
@@ -51,9 +51,10 @@ def import_model(file_path, dtype, dimensions=None, mode='r', order='C'):
     ```
     """
     
-    filename, extension = os.path.splitext(os.path.basename(file_path))
-    params_filename = filename+'.json'
-    print(f"Parameters filename: {params_filename}")
+    # filename, extension = os.path.splitext(os.path.basename(file_path))
+    # params_filename = filename+'.json'
+    # print(f"Parameters filename: {params_filename}")
+    params_filename = mk_paramsfile(file_path)
 
     # Ensure dimensions is a dictionary
     if dimensions is None:
@@ -114,7 +115,7 @@ def import_model(file_path, dtype, dimensions=None, mode='r', order='C'):
     return model
 
 
-def import_tiff_sequence(directory, filename, dtype):
+def import_tiff_sequence(directory, filename, dtype, dimensions=None):
     """
     Import raw data from a binary file.
 
@@ -128,24 +129,44 @@ def import_tiff_sequence(directory, filename, dtype):
         Data type of the binary file. Valid values are 'raw' and 'binary'.
 
     """
+    # Ensure dimensions is a dictionary
+    if dimensions is None:
+        raise ValueError("dimensions must be a dictionary")
+    else:
+        n1, n2, n3 = get_model_dimensions(dimensions)
+    
+    
+    file_path = directory+filename
+    params_filename = mk_paramsfile(file_path)
 
     file_listing = skimage.io.imread_collection(f'{directory}/*.tif*')
     first_image = file_listing[0]
     rows, cols = first_image.shape
     slices = len(file_listing)
-    model = np.zeros((slices, rows, cols))
+    # model must be in the structure of (rows, cols, slices)
+    model = np.zeros((rows, cols, slices), dtype=dtype)
+    # import the slices into the model
     for m in range(slices):
-        model[m, :, :] = file_listing[m]
-    if dtype == 'uint8':
-        model = model.astype(np.int8)
-        mk_log(f'{filename} is uint8')
-    elif dtype == 'uint16':
-        mk_log(f'{filename} is uint16')
-    else:
-        raise IOError(f"Unable to read file: {filename}")
+        model[:, :, m] = file_listing[m]
+        if dtype == 'uint8':
+            model = model.astype(np.uint8)
+        elif dtype == 'uint16':
+            model = model.astype(np.uint16)
+        elif dtype == 'float32':
+            model = model.astype(np.float32)
+        else:
+            raise ValueError(f"Unsupported data type: {dtype}. Please use 'uint8', 'uint16', or 'float32'.")
+        
 
     # Check wrong label numbering
     model = check_binary(model, filename=filename)
+
+    model = reshape_model(model, n1, n2, n3)
+    
+    update_parameters_file(paramsfile=params_filename,dim=model.ndim)
+    update_parameters_file(paramsfile=params_filename,dtype=dtype)
+    update_parameters_file(paramsfile=params_filename, nx=model.shape[0], ny=model.shape[1], nz=model.shape[2])
+    
 
     return model
 
@@ -174,3 +195,81 @@ def import_tif_model(filename):
 #         # Iterate over each slice and write it to the TIFF file
 #         for slice_idx in range(model.shape[0]):
 #             tif.save(model[slice_idx], compress=6)  # Adjust compress level as needed
+
+
+def export_model(filename, data, dtype='>f4', order='F'):
+    """
+    Writes moduli data to a binary file in big-endian or little-endian format with Fortran or C ordering.
+
+    Args:
+        filename (str): The path to the directory where the binary file will be created.
+        data (numpy.ndarray): The moduli data to be written.
+        dtype (str, optional): The data type of the elements in the array. Defaults to '>f4'.
+        mode (str, optional): The file mode used for opening the binary file.
+            - 'w+': Open for reading and writing, create the file if it does not exist.
+            - 'wb+': Open for reading and writing in binary mode, create the file if it does not exist.
+        order (str, optional): The memory layout of the data.
+            - 'F' for Fortran ordering (column-major),
+            - 'C' for C ordering (row-major).
+            Defaults to 'F'.
+
+    Returns:
+        None
+
+    Notes:
+        - The 'dtype' parameter specifies the byte order of the data. '>f4' stands for big-endian, and '<f4' stands for little-endian.
+        - The 'mode' parameter determines the file opening mode. The 'b' flag in 'wb+' ensures binary mode.
+        - The 'order' parameter determines the memory layout of the data, with 'F' for Fortran ordering and 'C' for C ordering.
+
+    Examples:
+        To write data in little-endian format with C ordering:
+        ```python
+        export_endian('output.bin', my_data, dtype='<f4', mode='wb+', order='C')
+        ```
+    """
+    
+    output_path = check_output_folder()
+    
+    file_path = os.path.join(output_path, filename)
+    
+    # Ensure that the array is dtype='>f4' or the specified dtype
+    nx, ny, nz, dim = np.shape(data)
+    data_tmp = np.zeros((nz, nx, ny, dim), dtype=dtype)
+    # convert the date into data_tmp
+    data_tmp[:, :, :, :] = data[:, :, :, :]
+    
+    data = data_tmp
+    model_reshaped = data.reshape(data.size, order=order)
+    model_reshaped.T.tofile(file_path)
+    
+    print(f"data saved: {file_path}")
+    export_header(file_path, data)
+    
+def export_header(filename, data):
+    # Extract the dimensions from the shape of the input data
+    n1, n2, n3, n4 = [format(dim, '06') for dim in data.shape]
+    
+    # Add the conditional statement
+    if n3 == '000001':
+        n3 = n4
+        n4 = '1'
+
+    # Define the dimensions
+    dim = [n1, n2, n3, n4]
+
+    # Define the header
+    header = "SEPlib Headerfile: quick & dirty: please always check !!!\n"
+    header += "so far: only n1,n2,n3,n4 will be part of output\n\n"
+    if filename == 'moduli':
+        header += f"sets next: in=\"moduli\"\n\n"
+    else:
+        header += f"sets next: in=\"./{filename}moduli\"\n\n"
+    header += f"n1={n1}\nn2={n2}\nn3={n3}\nn4={n4}\n"
+
+
+    filename = filename+'header'
+    # Write the header and dimensions to a new file
+    with open(filename, 'w') as f:
+        f.write(header)
+
+    return print(f"header saved: {filename}")
