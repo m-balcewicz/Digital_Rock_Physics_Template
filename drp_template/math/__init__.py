3d74492c061d04d32f1992c4a53379bd016fdf4c
m-balcewicz <martin.balcewicz@rockphysics.org>
2024-05-27 10:14:18 +0200

    feat: Add function to reorder labels in segmented image

--- drp_template/math/__init__.py
+++ drp_template/math/__init__.py
@@ -1,3 +1,3 @@
-from ._funcs import get_connected_porosity,get_phase_fractions,label_binary
+from ._funcs import get_connected_porosity,get_phase_fractions,label_binary, reorder_labels
 from ._effective_medium import density_solid_mix,density_fluid_mix,Brie_law,get_normalized_f_solid, bound, bound2
 from ._conversions import GPa2Pa, Pa2GPa
