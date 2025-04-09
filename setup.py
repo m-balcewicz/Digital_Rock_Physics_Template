432fc542c32b56405653f3f45fce0c4ba5cc3d32
m-balcewicz <martin.balcewicz@rockphysics.org>
2025-04-09 16:04:50 +0200

    changing dependencies for initial installation

--- setup.py
+++ setup.py
@@ -6,16 +6,12 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy>=1.24.0',
-        'matplotlib>=3.7.2, <3.8.2',
+        'matplotlib>=3.7.2',
         'cmcrameri~=1.7',
         'pandas>=2.0.0',
         'scikit-image~=0.20.0',
-        'vtk~=9.2.6',
         'tifffile~=2023.4.12',
-        'pillow~=10.0.1',
-        'porespy~=2.3.0',
-        'openpnm~=3.3.0',
-        'setuptools~=68.2.2',
+        'h5py>=2.2.3'
     ],
     python_requires='>=3.0, <4',
 )
