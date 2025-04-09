e9fc6930d8f9889720b0b1c9f5492ae64ee9a2dd
m-balcewicz <martin.balcewicz@rockphysics.org>
2025-04-09 16:14:58 +0200

    disabled 'get_connected_porosity'

--- drp_template/math/_funcs.py
+++ drp_template/math/_funcs.py
@@ -17,39 +17,39 @@ __all__ = [
 
 
 def get_connected_porosity(data, paramsfile='parameters.json'):
-    # Initialization
-    nx = read_parameters_file(paramsfile=paramsfile, paramsvars='nx')
-    ny = read_parameters_file(paramsfile=paramsfile, paramsvars='ny')
-    nz = read_parameters_file(paramsfile=paramsfile, paramsvars='nz')
+    # # Initialization
+    # nx = read_parameters_file(paramsfile=paramsfile, paramsvars='nx')
+    # ny = read_parameters_file(paramsfile=paramsfile, paramsvars='ny')
+    # nz = read_parameters_file(paramsfile=paramsfile, paramsvars='nz')
 
-    image3DConnected = np.ones((nx, ny, nz), dtype=np.uint8)
+    # image3DConnected = np.ones((nx, ny, nz), dtype=np.uint8)
 
-    # Step 1: Labeling the pores
-    # Inverse grains <-> pores
-    image3DInverse = np.abs(1 - data)
-    poreLabel = label(image3DInverse, connectivity=1)
+    # # Step 1: Labeling the pores
+    # # Inverse grains <-> pores
+    # image3DInverse = np.abs(1 - data)
+    # poreLabel = label(image3DInverse, connectivity=1)
 
-    print(poreLabel)
-    # image3DInverseLabel = poreLabel
-    #
-    # # Step 2: Find the label number that exist on both ends
-    # tempFirstSlide = image3DInverseLabel[:, :, 0]
-    # tempLastSlide = image3DInverseLabel[:, :, -1]
-    #
-    # labelFirstSlide = np.unique(tempFirstSlide)
-    # labelLastSlide = np.unique(tempLastSlide)
-    # labelEffective = np.intersect1d(labelFirstSlide, labelLastSlide)
-    #
-    # # Step 3: Create connected pore space
-    # nLabel = len(labelEffective)
-    # for i in range(nLabel):
-    #     lbl = labelEffective[i]
-    #     if lbl >= 1:  # Pore = 1+ -> 0
-    #         image3DConnected[image3DInverseLabel == lbl] = 0
-    #     else:  # Grain = 0 -> 1
-    #         image3DConnected[image3DInverseLabel == lbl] = 1
+    # print(poreLabel)
+    # # image3DInverseLabel = poreLabel
+    # #
+    # # # Step 2: Find the label number that exist on both ends
+    # # tempFirstSlide = image3DInverseLabel[:, :, 0]
+    # # tempLastSlide = image3DInverseLabel[:, :, -1]
+    # #
+    # # labelFirstSlide = np.unique(tempFirstSlide)
+    # # labelLastSlide = np.unique(tempLastSlide)
+    # # labelEffective = np.intersect1d(labelFirstSlide, labelLastSlide)
+    # #
+    # # # Step 3: Create connected pore space
+    # # nLabel = len(labelEffective)
+    # # for i in range(nLabel):
+    # #     lbl = labelEffective[i]
+    # #     if lbl >= 1:  # Pore = 1+ -> 0
+    # #         image3DConnected[image3DInverseLabel == lbl] = 0
+    # #     else:  # Grain = 0 -> 1
+    # #         image3DConnected[image3DInverseLabel == lbl] = 1
 
-    return poreLabel
+    return print('this function is not implemented yet')
 
 
 def get_phase_fractions(data, labels=None, paramsfile='parameters.json', log=False):
