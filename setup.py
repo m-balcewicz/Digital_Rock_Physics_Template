e0eeacb53e08ea79a07dcf785bbb6819e7e9c753
m-balcewicz <martin.balcewicz@rockphysics.org>
2025-11-03 15:22:37 +0100

    need of imagecodecs for tiff import

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ setup(
         'pandas>=2.0.0',
         'scikit-image~=0.20.0',
         'tifffile~=2023.4.12',
-        'h5py>=2.2.3'
+        'imagecodecs>=2023.1.23',
+        'h5py>=2.2.3',
+        'jsonschema>=4.0.0'
     ],
     python_requires='>=3.0, <4',
 )
