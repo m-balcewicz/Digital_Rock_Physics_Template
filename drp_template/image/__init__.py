ab4909b046290edac3836e38e27129999fb6441c
m-balcewicz <martin.balcewicz@rockphysics.org>
2025-11-11 19:51:23 +0100

    feat: add get_phase_color_resources import to image module

--- drp_template/image/__init__.py
+++ drp_template/image/__init__.py
@@ -2,3 +2,4 @@ from .slicing import ortho_slice, ortho_views, add_slice_reference_lines
 from .plotting import histogram, plot_effective_modulus, save_figure, get_figure_colors, plot_velocity_vs_angle
 from .rendering import volume_rendering, get_lighting_preset
 from .animation import create_rotation_animation
+from .colormaps import get_phase_color_resources
